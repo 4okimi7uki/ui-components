ee50bdbdcc21c03bd370fa9815d4a0f393760614
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-10-05 00:46:09 +0900

    style: adjust border color

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -22,10 +22,10 @@ export function InputField({ value, placeholder, onChange, required, className, 
                 {...props}
             />
             <div className="w-full flex justify-center relative">
-                <div data-info="bar" className="bg-[#333] w-full h-[1px] absolute top-[2px]"></div>
+                <div data-info="bar" className="bg-[var(--foreground)] w-full h-[1px] absolute top-[2px]"></div>
                 <div
                     data-info="bar"
-                    className="bg-pink-500 w-0 h-[3px] group-focus-within:w-full transition-all duration-200 absolute top-[1px] z-10"
+                    className="bg-blue-500 w-0 h-[3px] group-focus-within:w-full transition-all duration-200 absolute top-[1px] z-10"
                 ></div>
             </div>
         </div>
