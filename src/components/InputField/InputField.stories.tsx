53fa1275e1d3c2217f10da90de641d936a75ebf2
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-10-11 19:45:51 +0900

    fix: import path

--- src/components/InputField/InputField.stories.tsx
+++ src/components/InputField/InputField.stories.tsx
@@ -1,5 +1,5 @@
 import { InputField } from "@/components/InputField/InputField";
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { useState } from "react";
 
 const meta: Meta<typeof InputField> = {
